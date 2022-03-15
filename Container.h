41a47cefb41a9a8dfe6a7f6cc8f4a2db0e4a2895
kislya-tina <keipaqwerty@gmail.com>
2022-03-15 13:06:30 +0600

    box

--- Container.h
+++ Container.h
@@ -6,6 +6,7 @@
 #define ZERO_CONTAINER_H
 #include <vector>
 #include <algorithm>
+#include <stdexcept>
 
 
 class Container {
@@ -21,18 +22,6 @@ public:
         this->width = width;
         this->height = height;
         this->maxWeight = maxWeight;
-    }
-
-    void deleteBox(int i){
-        boxVector.erase(boxVector.begin() + i - 1) ;
-    }
-
-    void addBox(Box box){
-        if(maxWeight > this->summaryWeight()){
-            boxVector.push_back(box);
-        }else{
-            throw
-        }
     }
 
     int countBoxes(){
@@ -55,12 +44,38 @@ public:
         }
         return sumVal;
     }
+
     Box getBox(int i){
         Box box;
         return box = boxVector.at(i);
     }
 
+    void deleteBox(int i){
+        boxVector.erase(boxVector.begin() + i - 1) ;
+    }
 
+    int addBox(Box box){
+        if(maxWeight > this->summaryWeight() + box.getWeight()){
+            boxVector.push_back(box);
+        }else{
+            throw range_error("Коробка не влезет в контейнер по весу");
+        }
+        return boxVector.size() - 1;
+    }
+
+    friend istream& operator>>(istream& s, Container& obj) {//#7.1
+        s >> obj.lenght;
+        s >> obj.width;
+        s >> obj.height;
+        s >> obj.maxWeight;
+        return s;
+    }
+
+    friend ostream& operator<<(ostream& out, Container& obj) {//#7.2
+        out << "Size:(" << obj.lenght << ", " << obj.width << ", " << obj.height <<
+            ")cm Max weight:("<< obj.maxWeight<< ")kg" << endl;
+        return out;
+    }
 };
 
 
